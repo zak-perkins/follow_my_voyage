b416b5cad7ed7ce0154ab83488d9380908993706
Isaac Perkins <isaac.perkins.1@gmail.com>
2021-06-19 16:55:04 +1000

    honme page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,6 +8,7 @@ import 'comments_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   static String id = 'login_screen';
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
@@ -21,64 +22,67 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body:  ModalProgressHUD(
+      body: ModalProgressHUD(
         inAsyncCall: showSpinner,
         child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              height: 200.0,
-              child: Image.asset('images/logo.png'),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Email'),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Password'),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-          OnboardButton(
-            title: 'Sign In',
-            color: Colors.lightBlueAccent,
-            onPressed: () async {
-              setState(() {
-                showSpinner = true;
-              });
-              try {
-                final newUser = await _auth.signInWithEmailAndPassword(email: email, password: password);
-                if(newUser != null) {
-                  Navigator.pushNamed(context, CommentsScreen.id);
-                }
-                setState(() {
-                  showSpinner = false;
-                });
-              } catch(e) {
-                print(e);
-              }
-            },
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Enter Email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Enter Password'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              OnboardButton(
+                title: 'Sign In',
+                color: Colors.lightBlueAccent,
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final newUser = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, CommentsScreen.id);
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ],
           ),
-          ],
         ),
-      ),
       ),
     );
   }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,64 +1,164 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
 
 import 'package:follow_my_voyage/widgets/signin_button.dart';
 import 'login_screen.dart';
 import 'registration_screen.dart';
 
-
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
-  @override
 
+  @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
+
+    var screenSize = MediaQuery.of(context).size;
+    var tileWidth = (screenSize.width / 4) - 10.0;
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
-                ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            OnboardButton(
-              title: 'Sign In',
-              color: Colors.lightBlueAccent,
-              onPressed: () {
-                Navigator.pushNamed(context, LoginScreen.id);
-              },
-            ),
-            OnboardButton(
-              title: 'Sign Up',
-              color: Colors.lightBlue,
-              onPressed: () {
-                Navigator.pushNamed(context, RegistrationScreen.id);
-              },
-            ),
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.black12,
+        shadowColor: Colors.grey,
+        title: Container(
+          width: 40,
+          child: Image.asset('images/logo.png'),
         ),
+        actions: <Widget>[
+          IconButton(
+            tooltip: 'Sign Up',
+            icon: Icon(CupertinoIcons.person_add),
+            onPressed: () {
+              Navigator.pushNamed(context, RegistrationScreen.id);
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+          ),
+          IconButton(
+            tooltip: 'Sign In',
+            icon: Icon(CupertinoIcons.person_circle),
+            onPressed: () {
+              Navigator.pushNamed(context, LoginScreen.id);
+            },
+          ),
+        ],
       ),
+      body: Stack(children: <Widget>[
+        Container(
+          child: Image.asset(
+            'images/cover.jpg',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+          ),
+        ),
+        Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                  Text(
+                    'FollowMy.Voyage',
+                    style: TextStyle(
+                      fontSize: 36.0,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    OnboardButton(
+                      title: 'Sign Up',
+                      color: Colors.lightBlueAccent,
+                      onPressed: () {
+                        Navigator.pushNamed(context, RegistrationScreen.id);
+                      },
+                    ),
+                    SizedBox(
+                      width: 40.0,
+                    ),
+                    OnboardButton(
+                      title: 'Sign In',
+                      color: Colors.lightBlue,
+                      onPressed: () {
+                        Navigator.pushNamed(context, LoginScreen.id);
+                      },
+                    ),
+                  ]),
+              SizedBox(
+                height: 120.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: tileWidth,
+                    child: Text(
+                      'One',
+                      style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white30,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: tileWidth,
+                    child: Text(
+                      'Two',
+                      style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white30,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: tileWidth,
+                    child: Text(
+                      'Three',
+                      style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white30,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: tileWidth,
+                    child: Text(
+                      'Four',
+                      style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              ),
+            ]),
+      ]),
     );
   }
 }
-
